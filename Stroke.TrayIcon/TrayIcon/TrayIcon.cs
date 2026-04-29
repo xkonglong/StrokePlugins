0aad750f909a3bcebbd8aa19610f84426b0a30b2
poerin <poerin@live.com>
2026-04-29 18:27:03 +0800

    2026-04-29-3

--- Stroke.TrayIcon/TrayIcon/TrayIcon.cs
+++ Stroke.TrayIcon/TrayIcon/TrayIcon.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Globalization;
 using System.Reflection;
 using System.Windows.Forms;
@@ -12,15 +13,20 @@ namespace Stroke
         private static ToolStripMenuItem _pauseItem;
         private static bool _paused;
         private static bool _initialized;
+        private static Icon _normalIcon;
+        private static Icon _grayIcon;
 
         public static void SetVisibility(bool visible)
         {
             if (!_initialized)
             {
                 bool zh = CultureInfo.CurrentUICulture.Name.StartsWith("zh");
+                _normalIcon = Icon.ExtractAssociatedIcon(Assembly.GetEntryAssembly().Location) ?? SystemIcons.Application;
+                _grayIcon = CreateGrayIcon(_normalIcon);
+
                 _icon = new NotifyIcon
                 {
-                    Icon = Icon.ExtractAssociatedIcon(Assembly.GetEntryAssembly().Location) ?? SystemIcons.Application,
+                    Icon = _normalIcon,
                     Text = "Stroke"
                 };
 
@@ -31,11 +37,13 @@ namespace Stroke
                     if (_paused)
                     {
                         MouseHook.StartHook();
+                        _icon.Icon = _normalIcon;
                         _pauseItem.Text = zh ? "暂停" : "Pause";
                     }
                     else
                     {
                         MouseHook.StopHook();
+                        _icon.Icon = _grayIcon;
                         _pauseItem.Text = zh ? "恢复" : "Resume";
                     }
 
@@ -71,6 +79,31 @@ namespace Stroke
 
             if (_icon != null)
                 _icon.Visible = visible;
+        }
+
+        private static Icon CreateGrayIcon(Icon original)
+        {
+            using (Bitmap originalBitmap = original.ToBitmap())
+            {
+                Bitmap grayBitmap = new Bitmap(originalBitmap.Width, originalBitmap.Height);
+                using (Graphics graphics = Graphics.FromImage(grayBitmap))
+                {
+                    ColorMatrix colorMatrix = new ColorMatrix(new float[][]
+                    {
+                        new float[] {0.3f, 0.3f, 0.3f, 0, 0},
+                        new float[] {0.59f, 0.59f, 0.59f, 0, 0},
+                        new float[] {0.11f, 0.11f, 0.11f, 0, 0},
+                        new float[] {0, 0, 0, 1, 0},
+                        new float[] {0, 0, 0, 0, 1}
+                    });
+                    using (ImageAttributes attributes = new ImageAttributes())
+                    {
+                        attributes.SetColorMatrix(colorMatrix);
+                        graphics.DrawImage(originalBitmap, new Rectangle(0, 0, grayBitmap.Width, grayBitmap.Height), 0, 0, originalBitmap.Width, originalBitmap.Height, GraphicsUnit.Pixel, attributes);
+                    }
+                }
+                return Icon.FromHandle(grayBitmap.GetHicon());
+            }
         }
     }
 }
